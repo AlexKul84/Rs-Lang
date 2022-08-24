ddb9a63c2e73df86057d8e0debb092dc4d36a1f3
Alexey Kulikov <alexeykulikov30@gmail.com>
2022-08-24 22:36:22 +0300

    feat: add initial render audiocall game

--- src/app/pages/audiocall/audioCall.ts
+++ src/app/pages/audiocall/audioCall.ts
@@ -1,12 +1,70 @@
 import Component from "../../../common/Component";
+import { IWordsData } from '../../../asset/utils/types'
+import { URL } from '../../../asset/utils/types'
 
 class AudioCall extends Component {
+  game: Component<HTMLElement>;
+  currentChapter: number;
+  currentPage: number;
+  currentPageIndex: number;
+  chapterHard: Component<HTMLElement>;
+  gameWrapper: Component<HTMLElement>;
+
+
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', 'audio-call');
-    
+    this.currentChapter = 0;
+    this.currentPage = 0;
+    this.currentPageIndex = 1;
+    this.getItemWord(`${URL.url}${URL.group}${this.currentChapter}${URL.page}${this.currentPage}`);
+
     const title = new Component(this.node, 'h2', '', 'Audio Call игра');
 
+    this.game = new Component(this.node, 'div', 'game');
+
+    const chapters = new Component(this.node, 'div', 'chapters')
+    for (let i = 0; i < 6; i++) {
+      const chapter = new Component(chapters.node, 'div', 'chapter', `Раздел ${i + 1}`)
+      chapter.node.onclick = () => {
+        this.currentChapter = i;
+        this.currentPage = 0;
+        this.currentPageIndex = 1;
+
+        this.getItemWord(`${URL.url}${URL.group}${this.currentChapter}${URL.page}${this.currentPage}`)
+      }
+    }
   }
+
+
+  private getItemWord(url: string) {
+    this.getWord(url).then((data) => {
+      console.log(data[5]);
+
+      const data1 = data[this.getRandomePage()]
+      const data2 = data
+      const voice = new Component(this.game.node, 'div', 'voice');
+      voice.node.onclick = () => {
+        new Audio(`https://rss-lang-backends.herokuapp.com/${data1.audio}`).play();
+      }
+      const answer = new Component(this.game.node, 'div', 'answer');
+      const wordsToGuess = new Component(this.game.node, 'ul', 'words-to-guess');
+      const word1 = new Component(wordsToGuess.node, 'li', 'word-to-guess', `${data1.wordTranslate}`);
+      const word2 = new Component(wordsToGuess.node, 'li', 'word-to-guess', `${data2[this.getRandomePage()].wordTranslate}`);
+      const word3 = new Component(wordsToGuess.node, 'li', 'word-to-guess', `${data2[this.getRandomePage()].wordTranslate}`);
+      const word4 = new Component(wordsToGuess.node, 'li', 'word-to-guess', `${data2[this.getRandomePage()].wordTranslate}`);
+      const nextWord = new Component(this.game.node, 'button', 'next-word', 'Пропустить');
+
+    })
+  }
+
+  private getRandomePage = () => Math.round(0 - 0.5 + Math.random() * (29 - 0 + 1))
+
+
+  private async getWord(url: string) {
+    const resp = await fetch(url);
+    return resp.json();
+  }
+
 }
 
 export default AudioCall;
